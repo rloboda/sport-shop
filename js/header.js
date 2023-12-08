7378a5364281336355947673a8d76a345a0463f5
Roman Loboda <lobodaroman777@gmail.com>
2023-12-08 02:18:59 +0100

    added footer

--- js/header.js
+++ js/header.js
@@ -1,14 +1,10 @@
 const body = document.querySelector("body");
 const header = document.querySelector(".header");
 const btnSub = document.querySelector(".header__menu-btn");
- 
+
 
 btnSub.addEventListener("click", () => {
     body.classList.toggle("fixed");
     header.classList.toggle("responsive");
 })
 
-navLinks.addEventListener("click", () => {
-    body.classList.remove("fixed");
-    header.classList.remove("responsive");
-})
